6266756c116d518000e3c16e7a7ce1ec5b2039e6
Besim91 <78695114+Besim91@users.noreply.github.com>
2024-06-14 17:16:09 +0200

    Add player bar

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from './../../models/game';
+import { PlayerComponent } from './../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss'] 
 })
-
-
-
-export class GameComponent {
-
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game!: Game;
+  currentCard: string = '';  
 
-  takeCard() {
-    this.pickCardAnimation = true;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
   }
 
+  takeCard() {
+    if (!this.pickCardAnimation) {  
+      let card = this.game.stack.pop();
+
+      if (card !== undefined) {  
+        this.currentCard = card;
+      }
+    }
+    this.pickCardAnimation = true;
+
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    }, 1000);
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
 }
